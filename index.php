f0ea46f619078557c816422541d4db2dbf90c511
MiguelJR23 <r0b3rts2332@gmail.com>
2026-04-30 17:53:10 -0300

    Update title and form structure in index.php

--- index.php
+++ index.php
@@ -3,16 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina de Calculo de Salario</title>
+    <title>Calculo do salario</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $horas = $_POST["txthoras"];
-            $valor = $_POST["txtvalor"];
-            $salario = $horas * $valor;
-            echo "O salario é: R$ " . number_format($salario, 2, ",", ".");
-        }
-    ?>
+
+    <form action="calcularsalario.php" method="post">
+        <h1>Cálculo de Salário</h1>
+
+        <label>
+            Digite as horas trabalhadas:
+            <input type="text" name="txthoras">
+        </label>
+
+        <label>
+            Digite o valor da hora:
+            <input type="text" name="txtvalor">
+        </label>
+
+        <div class="botoes">
+            <input type="submit" value="Calcular">
+            <input type="reset" value="Limpar">
+        </div>
+    </form>
+
 </body>
 </html>
