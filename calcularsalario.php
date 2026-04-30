459fab5604bb8dff0f05df68aa7f36680ca34dbd
MiguelJR23 <r0b3rts2332@gmail.com>
2026-04-30 17:53:42 -0300

    Update calcularsalario.php

--- calcularsalario.php
+++ calcularsalario.php
@@ -3,16 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina de Calculo de Salario</title>
+    <title>Resultado do salário</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $horas = $_POST["txthoras"];
-            $valor = $_POST["txtvalor"];
-            $salario = $horas * $valor;
-            echo "O salario é: R$ " . number_format($salario, 2, ",", ".");
-        }
-    ?>
+
+    <div class="resultado">
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $horas = $_POST["txthoras"];
+                $valor = $_POST["txtvalor"];
+                $salario = $horas * $valor;
+
+                echo "<h1>Resultado</h1>";
+                echo "<p>O salário é:</p>";
+                echo "<strong>R$ " . number_format($salario, 2, ",", ".") . "</strong>";
+            }
+        ?>
+    </div>
+
 </body>
 </html>
